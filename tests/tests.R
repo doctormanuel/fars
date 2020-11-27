9332cc963c818e32d4d8fd446939464040c38570
doctormanuel <manuprinceps@gmail.com>
2020-11-26 22:32:24 -1000

    last

--- tests/tests.R
+++ tests/tests.R
@@ -1,8 +1,7 @@
 library(testthat)
 library(fars)
-testthat::test_that("FARS test",{
-        expect_that(fars::fars_read_years(2015),is_a("list"))
-})
-#expect_that(fars_summarize_years(2015),is_a("tbl_df"))
-#expect_that(fars_read_years(2015),is_a("list"))
-#expect_that(make_filename(2015),is_a("character"))
+#testthat::test_that("FARS test",{
+#        expect_that(fars::fars_read_years(2015),is_a("list"))
+#})
+expect_that(fars_summarize_years(2015),is_a("tbl_df"))
+expect_that(fars_read_years(2015),is_a("list"))
